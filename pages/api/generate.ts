0f21be97c5666e1c09a7e5fb235d15e43203bc75
kypkk <kypss50102@gmail.com>
2023-05-03 22:49:16 +0800

    feat: generate_image api

---
+++ pages/api/generate.ts
@@ -0,0 +1,33 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+
+const openai = new OpenAIApi(configuration);
+
+interface CustomResponseType {
+  result: string | undefined;
+}
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<CustomResponseType>
+) {
+  console.log(process.env.OPENAI_API_KEY);
+  try {
+    const results = await openai.createImage({
+      prompt: "chatgpt",
+      n: 1,
+      size: "1024x1024",
+    });
+
+    console.log(results.data.data[0]);
+    res.status(200).json({ result: results.data.data[0].url });
+  } catch (e: any) {
+    res.json(e);
+  }
+  // res.json({ result: "test" });
+}
